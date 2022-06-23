cb765e00a798388a9d2c0625c7c9f7ef3104c98c
Samira Marcela Roman <samira1980dev@gmail.com>
2022-06-23 11:48:42 -0300

    Deafio 01 de array

--- JavaCompleto/src/arraysEcollections/Desafio001.java
+++ JavaCompleto/src/arraysEcollections/Desafio001.java
@@ -1,5 +1,35 @@
 package arraysEcollections;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Desafio001 {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Quantas notas voce quer colocar? ");
+		int qtdNotas = entrada.nextInt();
+		
+		double notas [] = new double [qtdNotas];
+		
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Insira a nota " + (i + 1) + ": ");
+			notas[i] = entrada.nextInt();
+		}
+		
+		double total = 0;
+		for (double nota: notas) {
+			total += nota;
+		}
+		
+		System.out.println("A media eh : " + (total / notas.length));
+		
+		
+		
+		entrada.close();
+		
+	}
 
 }
